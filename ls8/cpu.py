ab0a571b5337efecbeae0dfa2779af23dee89072
bquizza5 <blaine555@gmail.com>
2020-01-27 14:44:03 -0700

    day 1 mvp

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,12 +2,29 @@
 
 import sys
 
+# instructions
+
+hlt = 0b00000001
+ldi = 0b10000010
+prn = 0b01000111
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256
+        self.reg = [0] * 8
+        self.pc = 0
+
+
+    def ram_read(self, location):
+        return self.ram[location]
+
+    def ram_write(self, value, location):
+        self.ram[location] = value
+        
+
 
     def load(self):
         """Load a program into memory."""
@@ -62,4 +79,25 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        running = True
+
+        while running:
+            IR = self.ram[self.pc]
+
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+
+            if IR == hlt:
+                sys.exit()
+
+            elif IR == ldi:
+                self.reg[operand_a] = operand_b
+                self.pc += 3
+
+            elif IR == prn:
+                print(self.reg[operand_a])
+                self.pc += 2
+
+            else:
+                print('bad IR', IR)
+                sys.exit()
